2eabd6706c6ea00f8e9eb87298df128191226c88
anatmnegri <anateresamn@gmail.com>
2022-03-17 21:01:17 -0300

    atualização Projeto API!

---
+++ src/app.js
@@ -0,0 +1,22 @@
+const express = require("express")
+const app = express()
+
+//rotas
+const index = require("./routes/index")
+
+app.use(express.json())
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    )
+    next()
+     // como criei uma função dentro do app.use, preciso dar um "next()" para mandar ele seguir para a próxima middleware. 
+    // se eu não faço isso, a requisição vai ficar travada aí.
+})
+
+app.use("/", index)
+
+module.exports = app
